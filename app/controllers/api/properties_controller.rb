1da2ced179fa05c7827f3cf8be322c90533132de
James Yeates <jimibue@gmail.com>
2020-10-13 11:09:03 -0600

    backend pagination basic setup

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -1,5 +1,14 @@
 class Api::PropertiesController < ApplicationController
+  before_action :set_page
+
   def index
-    render json: Property.available
+    render json: Property.page(@page).available
+    # render json: Property.available
+  end
+
+  private
+
+  def set_page
+    @page = params[:page] || 1
   end
 end
