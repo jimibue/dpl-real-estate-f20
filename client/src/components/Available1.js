6dd8caa7b7ec1be3061c19098ca8d80766524f81
James Yeates <jimibue@gmail.com>
2020-10-13 12:31:59 -0600

    yo

--- client/src/components/Available1.js
+++ client/src/components/Available1.js
@@ -4,131 +4,131 @@ import React, { useState, useEffect } from "react";
 import { List, Table } from "semantic-ui-react";
 import InfiniteScroll from "react-infinite-scroller";
 
-// const Available = () => {
-//   const [agents, setAgents] = useState([]);
-//   const [page, setPage] = useState(1);
-//   const [totalPages, setTotalPages] = useState(5);
+const Available = () => {
+  const [agents, setAgents] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(5);
 
-//   // async await demo
-//   // const getAgents = async () => {
-//   //   try {
-//   //     let res = await Axios.get("/api/properties");
-//   //     setAgents(res.data);
-//   //   } catch (err) {
-//   //     alert("error occured getting agents properties");
-//   //   }
-//   // };
+  // async await demo
+  // const getAgents = async () => {
+  //   try {
+  //     let res = await Axios.get("/api/properties");
+  //     setAgents(res.data);
+  //   } catch (err) {
+  //     alert("error occured getting agents properties");
+  //   }
+  // };
 
-//   // useEffect(() => {
-//   //   getAgents();
-//   // }, []);
+  // useEffect(() => {
+  //   getAgents();
+  // }, []);
 
-//   const normalizeData = (data) => {
-//     let agents = [];
+  const normalizeData = (data) => {
+    let agents = [];
 
-//     //create an array of unique agent_ids
-//     let ids = [...new Set(data.map((d) => d.agent_id))];
+    //create an array of unique agent_ids
+    let ids = [...new Set(data.map((d) => d.agent_id))];
 
-//     // loop over ids grab all the properties where agent = id
-//     ids.forEach((id) => {
-//       // grab all properties that belong to the agent (ie id)
-//       let properties = data.filter((d) => d.agent_id == id);
+    // loop over ids grab all the properties where agent = id
+    ids.forEach((id) => {
+      // grab all properties that belong to the agent (ie id)
+      let properties = data.filter((d) => d.agent_id == id);
 
-//       let { agent_id, first_name, last_name } = properties[0];
+      let { agent_id, first_name, last_name } = properties[0];
 
-//       // just create property object that don't have agent_id, first_name, last_name
-//       let agentProperties = properties.map((p) => {
-//         let { price, beds, baths, sq_ft, city, street, zip, id } = p;
-//         return { price, beds, baths, sq_ft, city, street, zip, id };
-//       });
+      // just create property object that don't have agent_id, first_name, last_name
+      let agentProperties = properties.map((p) => {
+        let { price, beds, baths, sq_ft, city, street, zip, id } = p;
+        return { price, beds, baths, sq_ft, city, street, zip, id };
+      });
 
-//       let detail = {
-//         agent_id,
-//         first_name,
-//         last_name,
-//         properties: agentProperties,
-//       };
+      let detail = {
+        agent_id,
+        first_name,
+        last_name,
+        properties: agentProperties,
+      };
 
-//       agents.push(detail);
-//     });
-//     return agents;
-//   };
+      agents.push(detail);
+    });
+    return agents;
+  };
 
-//   // useEffect(() => {
-//   //   console.log("useEffect called here");
-//   //   Axios.get("/api/properties")
-//   //     .then((res) => {
-//   //       let normalizedData = normalizeData(res.data);
-//   //       console.log(normalizedData);
-//   //       setAgents(normalizedData);
-//   //     })
-//   //     .catch((err) => {
-//   //       alert("error occured getting agents properties");
-//   //     });
-//   // }, []); // componentDIDMount[]
+  // useEffect(() => {
+  //   console.log("useEffect called here");
+  //   Axios.get("/api/properties")
+  //     .then((res) => {
+  //       let normalizedData = normalizeData(res.data);
+  //       console.log(normalizedData);
+  //       setAgents(normalizedData);
+  //     })
+  //     .catch((err) => {
+  //       alert("error occured getting agents properties");
+  //     });gits 
+  // }, []); // componentDIDMount[]
 
-//   const loadMore = async () => {
-//     // do axios call to get more data
-//     console.log("loadMore called here");
-//     try {
-//       let res = await Axios.get(`/api/properties?page=${page + 1}`);
-//       let normalizedData = normalizeData(res.data);
-//       setAgents([...agents, normalizedData]);
-//       setPage(page + 1);
-//     } catch (err) {
-//       alert("error in loadmore");
-//     }
-//   };
-//   return (
-//     <List style={{ height: "110px", overflow: "auto" }}>
-//       {page}
-//       <InfiniteScroll
-//         pageStart={page}
-//         loadMore={loadMore}
-//         hasMore={page < totalPages}
-//         hasMore={true}
-//         // useWindow={false}
-//       >
-//         {agents.map((agent) => (
-//           <List.Item key={agent.id + "a"}>
-//             <List.Header>{agent.first_name}</List.Header>
+  const loadMore = async () => {
+    // do axios call to get more data
+    console.log("loadMore called here");
+    try {
+      let res = await Axios.get(`/api/properties?page=${page + 1}`);
+      let normalizedData = normalizeData(res.data);
+      setAgents([...agents, normalizedData]);
+      setPage(page + 1);
+    } catch (err) {
+      alert("error in loadmore");
+    }
+  };
+  return (
+    <List style={{ height: "110px", overflow: "auto" }}>
+      {page}
+      <InfiniteScroll
+        pageStart={page}
+        loadMore={loadMore}
+        hasMore={page < totalPages}
+        hasMore={true}
+        // useWindow={false}
+      >
+        {agents.map((agent) => (
+          <List.Item key={agent.id + "a"}>
+            <List.Header>{agent.first_name}</List.Header>
 
-//             <Table celled>
-//               <Table.Header>
-//                 <Table.Row>
-//                   <Table.HeaderCell>Price</Table.HeaderCell>
-//                   <Table.HeaderCell>Beds</Table.HeaderCell>
-//                   <Table.HeaderCell>Baths</Table.HeaderCell>
-//                   <Table.HeaderCell>Sq. Ft.</Table.HeaderCell>
-//                   <Table.HeaderCell>Street</Table.HeaderCell>
-//                   <Table.HeaderCell>City</Table.HeaderCell>
-//                   <Table.HeaderCell>ZIP</Table.HeaderCell>
-//                 </Table.Row>
-//               </Table.Header>
-//               <Table.Body>
-//                 {agent.properties &&
-//                   agent.properties.map((p) => (
-//                     <Table.Row key={p.id + "p"}>
-//                       <Table.Cell>{p.price}</Table.Cell>
-//                       <Table.Cell>{p.beds}</Table.Cell>
-//                       <Table.Cell>{p.baths}</Table.Cell>
-//                       <Table.Cell>{p.sq_ft}</Table.Cell>
-//                       <Table.Cell>{p.street}</Table.Cell>
-//                       <Table.Cell>{p.city}</Table.Cell>
-//                       <Table.Cell>{p.zip}</Table.Cell>
-//                     </Table.Row>
-//                   ))}
-//                 {/*end of agent.agentProperties.map*/}
-//               </Table.Body>
-//             </Table>
-//           </List.Item>
-//         ))}
-//       </InfiniteScroll>
-//     </List>
-//   );
-// };
+            <Table celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Price</Table.HeaderCell>
+                  <Table.HeaderCell>Beds</Table.HeaderCell>
+                  <Table.HeaderCell>Baths</Table.HeaderCell>
+                  <Table.HeaderCell>Sq. Ft.</Table.HeaderCell>
+                  <Table.HeaderCell>Street</Table.HeaderCell>
+                  <Table.HeaderCell>City</Table.HeaderCell>
+                  <Table.HeaderCell>ZIP</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {agent.properties &&
+                  agent.properties.map((p) => (
+                    <Table.Row key={p.id + "p"}>
+                      <Table.Cell>{p.price}</Table.Cell>
+                      <Table.Cell>{p.beds}</Table.Cell>
+                      <Table.Cell>{p.baths}</Table.Cell>
+                      <Table.Cell>{p.sq_ft}</Table.Cell>
+                      <Table.Cell>{p.street}</Table.Cell>
+                      <Table.Cell>{p.city}</Table.Cell>
+                      <Table.Cell>{p.zip}</Table.Cell>
+                    </Table.Row>
+                  ))}
+                {/*end of agent.agentProperties.map*/}
+              </Table.Body>
+            </Table>
+          </List.Item>
+        ))}
+      </InfiniteScroll>
+    </List>
+  );
+};
 
-// export default Available;
+export default Available;
 
 export default function Available() {
   const [agents, setAgents] = useState([]);
