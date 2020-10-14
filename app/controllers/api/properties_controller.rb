31c2cf3abae34e3706546a6a9f880ceb8800d88a
James Yeates <jimibue@gmail.com>
2020-10-14 10:11:11 -0600

    controller setup

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -8,6 +8,17 @@ class Api::PropertiesController < ApplicationController
     # render json: Property.available
   end
 
+  def city
+    # on frontend the properties would come back as res.data in the .then()
+    # render json: Property.by_city(params[:city])
+
+    properties = Property.page(@page).by_city(params[:city])
+    total_pages = properties.total_pages
+
+    # on frontend the properties would come back as res.data.properties and we would also res.data.total_pages in the .then()
+    render json: { properties: properties, total_pages: total_pages }
+  end
+
   private
 
   def set_page
